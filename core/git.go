f90150a9e6889ce7c99f1c83f8e9e6c17f75fa6e
0xtavian <21030907+0xtavian@users.noreply.github.com>
2019-11-07 13:26:14 -0600

    Update git.go

--- core/git.go
+++ core/git.go
@@ -1,27 +1,33 @@
 package core
 
 import (
-	"context"
-	"time"
+        "context"
+        "time"
 
-	"gopkg.in/src-d/go-git.v4"
+        "gopkg.in/src-d/go-git.v4"
+        "gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 )
 
 func CloneRepository(session *Session, url string, dir string) (*git.Repository, error) {
-	localCtx, cancel := context.WithTimeout(session.Context, time.Duration(*session.Options.CloneRepositoryTimeout)*time.Second)
-	defer cancel()
+        localCtx, cancel := context.WithTimeout(session.Context, time.Duration(*session.Options.CloneRepositoryTimeout)*time.Second)
+        defer cancel()
 
-	repository, err := git.PlainCloneContext(localCtx, dir, false, &git.CloneOptions{
-		Depth:             1,
-		RecurseSubmodules: git.NoRecurseSubmodules,
-		URL:               url,
-		SingleBranch:      true,
-		Tags:              git.NoTags,
-	})
+        repository, err := git.PlainCloneContext(localCtx, dir, false, &git.CloneOptions{
+                Depth:             1,
+                RecurseSubmodules: git.NoRecurseSubmodules,
+                URL:               url,
+                SingleBranch:      true,
+                Tags:              git.NoTags,
+                Auth:              &http.BasicAuth{
+			Username: "{enter-GHE-username}",
+			Password: "{enter-GHE-token}",
+                },
+        })
 
-	if err != nil {
-		return nil, err
-	}
+        if err != nil {
+                return nil, err
+        }
 
-	return repository, nil
+        return repository, nil
 }
+
